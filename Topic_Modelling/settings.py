06d538fbd63f10e2f1b9309f3d250384b785a83e
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-04-22 20:32:23 -0400

    cleaned the codes

--- Topic_Modelling/settings.py
+++ Topic_Modelling/settings.py
@@ -1,17 +1,26 @@
-
+"""
+This class is collection of database constants
+"""
 
 class Settings:
-    def __init__(self):
-        pass
+    
+    # Database constants
 
     DATASET_FILE = '../dataset/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
+    BUSINESS_DATA_FILE = '../dataset/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
     MONGO_CONNECTION_STRING = "mongodb://localhost:27017/"
+    
+    # Database names
+    
     REVIEWS_DATABASE = "Dataset_Challenge_Reviews"
     TAGS_DATABASE = "Tags"
+    
+    # Collection names
+    
     REVIEWS_COLLECTION = "Reviews"
     CORPUS_COLLECTION = "Corpus"
     BUSINESS_COLLECTION = 'Business'
-    BUSINESS_DATA_FILE = '../dataset/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
+    
   
     # Dictionary Settings
     
@@ -19,6 +28,8 @@ class Settings:
     Corpus_path = "models/corpus.lda-c"
     Lda_num_topics = 60
     Lda_model_path = "models/lda_model_50_topics.lda"
+    
+    # Backup Collections
     
     REVIEWS_COLLECTION_BKP = "Reviews_Bkp"
     CORPUS_COLLECTION_BKP = "Corpus_Bkp"
